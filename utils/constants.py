fcce785ad980626bc3cdab1af4f338831d812da2
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-14 13:14:21 +0200

    Add constants module with PILOTS list and placeholder for datasets

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+PILOTS = ["Barcelona", "Gothenburg"]
+
+DATASETS = {
+    "Walking journeys": "trips_walking",
+    "Bike & PMV by sex": "bike_and_pmv_by_sex",
+    "Public accessibility": "streets_accessibility"
+}
