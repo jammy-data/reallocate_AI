79df998ccfe727a66d91724117a4aec645b53e67
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-17 17:05:14 +0200

    New format designed for st.pages

--- app.py
+++ app.py
@@ -1,34 +1,20 @@
 import streamlit as st
-from utils.data_loader import load_all_pilots
-from datetime import datetime
-# from utils.helpers import slugify  # We'll create this next
 
-st.set_page_config(page_title="Pilot Overview", layout="wide")
-st.title("🧭 Pilot Projects Overview")
+from st_pages import add_page_title, get_nav_from_toml, hide_pages
 
-pilots = load_all_pilots()
+st.set_page_config(layout="wide")
 
-for pilot in pilots:
-    name = pilot["name"]
-    city = pilot["city"]
-    title = pilot["title"]
-    description = pilot.get("description", "No description available.")
-    image_url = pilot.get("image", "")
-    start = datetime.strptime(pilot["start_date"], "%Y-%m-%d").date()
-    end = datetime.strptime(pilot["end_date"], "%Y-%m-%d").date()
+# hide_pages(["Pilot"])
 
-    with st.expander(f"📍 {name}"):
-        st.write(f"**City:** {city}")
-        st.write(f"**Pilot Title:** {title}")
-        st.write(f"**📅 Start:** {start}  📅 End:** {end}")
-        st.write(f"📝 **Description:** {description}")
-        st.image(image_url, caption=name, use_column_width="always")
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
-        # Navigate to detailed page
-        link = f"/1_Pilot?pilot={pilot['id']}"
-        st.markdown(
-            f"""<a href="{link}" target="_self">
-                <button style="margin-top: 1rem;">Show more</button>
-               </a>""",
-            unsafe_allow_html=True,
-        )
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+)
+
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
