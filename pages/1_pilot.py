ae096888378b20057ef9c7a89b1ea94203b94d92
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-17 14:39:31 +0200

    New page where we will have the information of the pilots

---
+++ pages/1_pilot.py
@@ -0,0 +1,32 @@
+# pages/1_Pilot.py
+import streamlit as st
+from utils.data_loader import load_pilot_data
+from components import header, footer, tab_info, tab_kpis, tab_dashboard
+
+# Get query param
+query_params = st.query_params
+pilot_id = query_params.get("pilot")
+
+if not pilot_id:
+    st.error("No pilot selected.")
+    st.stop()
+
+pilot = load_pilot_data(pilot_id)
+
+# Render header
+header.render(pilot)
+
+# Tabs
+tab1, tab2, tab3 = st.tabs(["Pilot Info", "KPIs", "Dashboard"])
+
+with tab1:
+    tab_info.render(pilot["tabs"]["info"])
+
+with tab2:
+    tab_kpis.render(pilot["tabs"]["kpis"])
+
+with tab3:
+    tab_dashboard.render(pilot["tabs"]["dashboard"])
+
+# Render footer
+footer.render(pilot["footer"])
