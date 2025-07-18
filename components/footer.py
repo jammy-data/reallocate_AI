ad89f36ede13651a61ac32554127daf4514be574
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-18 21:58:40 +0200

    Removed test code

--- components/footer.py
+++ components/footer.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from utils.layout import  show_logos_from_list
-from utils.data_loader import load_logos
 def render():
-    logos = load_logos()
-    st.write("### Footer")
-    st.write("This is the footer section of the page.")
-    st.write(logos)
     st.write("---")
     show_logos_from_list()  # Uses default LOGOS from layout    
     st.write("---")
