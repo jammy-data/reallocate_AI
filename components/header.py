d866b4bc38d45ab2f806942503eeb7850e51a1ea
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-18 19:57:51 +0200

    Adjusted image size

--- components/header.py
+++ components/header.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
 def render(pilot):
-    st.image(pilot["image"])
-    st.markdown(f"## {pilot['city']} — {pilot['title']}")
+
+    # st.image(
+    #     pilot["image"],
+    #     use_container_width=True,
+    # )
+    # st.markdown(f"## {pilot['city']} — {pilot['title']}")
+
+    st.markdown(
+        f"""
+        <img src="{pilot['image']}" 
+             style="width: 100%; max-height: 40vh; object-fit: cover; border-radius: 10px;">
+        <h2>{pilot['city']} — {pilot['title']}</h2>
+        """,
+        unsafe_allow_html=True
+    )
