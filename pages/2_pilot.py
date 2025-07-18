ec1c1d6f40a10e8c264e444fcb462b22312a9970
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-07-18 19:25:28 +0200

    Add event liscener that changes the tab based on the url

--- pages/2_pilot.py
+++ pages/2_pilot.py
@@ -1,5 +1,6 @@
 # pages/1_Pilot.py
 import streamlit as st
+from streamlit import components
 from utils.data_loader import load_pilot_data
 from components import header, footer, tab_info, tab_kpis, tab_dashboard
 
@@ -17,7 +18,8 @@ pilot = load_pilot_data(pilot_id)
 header.render(pilot)
 
 # Tabs
-tab1, tab2, tab3 = st.tabs(["Pilot Info", "KPIs", "Dashboard"])
+tab_names = ["Pilot Info", "KPIs", "Dashboard"]
+tab1, tab2, tab3 = st.tabs(tab_names)
 
 with tab1:
     tab_info.render(pilot["tabs"]["Pilot Info"])
@@ -28,5 +30,23 @@ with tab2:
 with tab3:
     tab_dashboard.render(pilot["tabs"]["Dashboard"])
 
+#Test change TAB
+query = st.query_params
+if "tab" in query:
+    tab_name = query["tab"]
+    if tab_name in tab_names:
+        index_tab = tab_names.index(tab_name)
+
+        # Simulate tab click via JS after page load
+        js_code = f"""
+        <script>
+            window.addEventListener('load', function () {{
+                const tab = window.parent.document.querySelectorAll('[data-baseweb="tab"]')[{index_tab}];
+                if (tab) tab.click();
+            }});
+        </script>
+        """
+        components.v1.html(js_code, height=0)
+    
 # Render footer
 # footer.render(pilot["footer"])
